1bf38115f23b7aa0bcf675a0a701ba404591498f
kikyoluka <1092834167@qq.com>
2020-06-03 11:31:08 +0800

    update

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-const RoomConfig = require('roomConfig')
+const labCtrl = require('labCtrl')
 
 module.exports.loop = function () {
     require('prototype.Creep.move')
     require('creep.prototype')
     require('stateScanner').stateScanner()
+    require('roomConfig').run()
     require('creep').CreepConfig.run()
     require('tower').TowerConfig.run()
     require('room').RoomConfig.run()
     require('pc').PcConfig.run()
 
+
     if (!Memory.RoomConfig) {
         Memory.RoomConfig = RoomConfig
     }
 
+    labCtrl.run('E49S15', RESOURCE_GHODIUM, 2000)
 }
