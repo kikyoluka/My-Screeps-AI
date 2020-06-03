0b36e4bfc53f636cc2292c3c4f3fcac60d2d3b44
kikyoluka <1092834167@qq.com>
2020-06-03 15:43:17 +0800

    紫妈

--- pcConfig.js
+++ pcConfig.js
@@ -1,5 +1,25 @@
 module.exports = {
-    work: function () {
+    run: function (roomName) {
+        let powerSpawn = Game.rooms[roomName].find(FIND_STRUCTURES, {
+            filter: (s) => s.structureType == STRUCTURE_POWER_SPAWN
+        })[0];
+
+        if (powerSpawn.store[RESOURCE_ENERGY] >= 2000 && powerSpawn.store[RESOURCE_POWER >= 50]) {
+            powerSpawn.processPower()
+        }
+
+        Game.powerCreeps['紫妈'].spawn(powerSpawn);
+
+        let pc = Game.powerCreeps['紫妈'];
+
+        if (pc.ticksToLive <= 100) {
+            pc.say(`在 ${pc.ticksToLive} tick后死亡 `)
+            if (pc.renew(powerSpawn) == ERR_NOT_IN_RANGE) {
+                pc.moveTo(powerSpawn)
+            }
+        }
+
+
 
     }
 }
